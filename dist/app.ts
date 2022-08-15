5d62fb1b0089b0978fefdd1c2c37d8ad922e2c65
zaham <zaham2@yahoo.com>
2022-08-15 17:33:54 +0200

    Added types... Minor fixes

--- dist/app.ts
+++ dist/app.ts
@@ -3,60 +3,12 @@ import bodyParser from "body-parser";
 import { google } from "googleapis";
 
 const app: express.Application = express();
-const port = 3000;
+const port: number = 3000;
 
 app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
-});
-
-app.post("/createSheet", async (req: Request, res: Response) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  const client = await auth.getClient();
-
-  const googleSheets = google.sheets({ version: "v4", auth: client });
-  try {
-    const spreadsheet = await googleSheets.spreadsheets.create({
-      fields: "spreadsheetId",
-    });
-
-    console.log("The id is " + spreadsheet.data.spreadsheetId);
-    res.send(spreadsheet.data.spreadsheetId);
-    return spreadsheet.data.spreadsheetId;
-  } catch (e) {
-    console.log("error");
-    res.send(e);
-  }
-});
-
-app.get("/readrows", async (req: Request, res: Response) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  const client = await auth.getClient();
-
-  const spreadsheetId = req.body.spreadsheetId;
-  const range = req.body.range;
-  const googleSheets = google.sheets({ version: "v4", auth: client });
-  try {
-    const result = await googleSheets.spreadsheets.values.get({
-      spreadsheetId,
-      range,
-    });
-    let rangeArr = range.split("!");
-    // @ts-ignore
-    res.send(rangeArr);
-  } catch (e) {
-    console.log("error");
-    res.send(e);
-  }
 });
 
 app.get("/filter", async (req: Request, res: Response) => {
@@ -70,7 +22,7 @@ app.get("/filter", async (req: Request, res: Response) => {
   const client = await auth.getClient();
 
 //   Now we access the body and query parameters
-  const spreadsheetId = req.body.spreadsheetId;
+  const spreadsheetId: string = req.body.spreadsheetId;
   let range = req.body.range;
   const filter = req.query.filter;
   const googleSheets = google.sheets({ version: "v4", auth: client });
@@ -78,7 +30,7 @@ app.get("/filter", async (req: Request, res: Response) => {
   let rangeArr = range.split("!");
   rangeArr[1] = rangeArr[1].split(":");
 
-  let headerRange = rangeArr[0]+"!"+"A1:Z1"
+  let headerRange: string = rangeArr[0]+"!"+"A1:Z1"
 
   try {
     const result = await googleSheets.spreadsheets.values.get({
@@ -86,31 +38,36 @@ app.get("/filter", async (req: Request, res: Response) => {
       range: headerRange,
     });
 
+    // Here we retrieve the top row from the sheet to use for filtering
     //   @ts-ignore
     let columnHeaders = result.data.values[0];
 
     //   Check that the filter exits
     //   And get the number of that column
-    let i;
+    let i: number;
     for (i = 0; i < columnHeaders.length; i++) {
       if (columnHeaders[i] === filter) {
         break;
       }
     }
     if (i === columnHeaders.length) {
-      console.log("Bad Filter");
-      res.send("Bad Filter"); //just return all data
+      res.send("Bad Filter");
       return;
     }
     // Now we know that this is a valid filter
     // So now we display the filtered data
 
     // First we convert i to the corresponding character
-    let columnToFilter = String.fromCharCode(65 + i); //A
-    let startRow = rangeArr[1][0].substring(1);
-    let endRow = rangeArr[1][1].substring(1);
+    let columnToFilter: string = String.fromCharCode(65 + i); //A
+
+    // Next we read the cell range
+    let startRow: string = rangeArr[1][0].substring(1);
+    let endRow: string = rangeArr[1][1].substring(1);
+    
+    // Now we append the new Range to our range string
+    let sheetName: string = rangeArr[0]
     range =
-      rangeArr[0] +
+      sheetName +
       "!" +
       columnToFilter +
       startRow +
@@ -138,5 +95,3 @@ export default app;
 // Google Sheets I used for testing
 // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit#gid=0
 // https://docs.google.com/spreadsheets/d/1L8uzJ1OlYnm1NPps3VM-GtjfypygNO89qeZ8zAY90aA/edit#gid=0
-
-
